cafafc288b35fc7053c401082bbb24da81756a6e
fjswayze <fjswayze@vassar.edu>
2020-10-16 13:54:03 -0700

    quiz

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { data } from 'autoprefixer';
-import _ from 'lodash'; 
+import _, { over } from 'lodash'; 
 
 
 
@@ -218,6 +218,148 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
     }
     setInterval(draw, 10); 
+
+  
+
+    // QUIZ
+
+
+    let questionIndex = 1; 
+    showQuestion(questionIndex); 
+
+    function plusQuestion(n) {
+        questionIndex += n 
+        showQuestion(questionIndex + 1)
+    }
+
+    function showQuestion(n) {
+        let i; 
+        var questions = document.getElementsByClassName('questions'); 
+        if (n > questions.length) {
+            questionIndex = 7
+        }
+        for (i = 0; i < questions.length; i++){
+            questions[i].style.display = 'none'
+        }
+        questions[questionIndex - 1].style.display = 'flex'
+    }
+    
+
+    let lastMan = 0; 
+    let regularMan = 0; 
+    let overMan = 0; 
+
+
+    document.getElementById('take-quiz').addEventListener('click', function(){
+        plusQuestion(1); 
+    })
+
+    document.getElementById('Q1A').addEventListener('click', function () {
+        plusQuestion(1);
+        lastMan += 1; 
+    })
+    document.getElementById('Q1B').addEventListener('click', function () {
+        plusQuestion(1);
+        regularMan += 1; 
+    })
+    document.getElementById('Q1C').addEventListener('click', function () {
+        plusQuestion(1);
+        overMan +=1; 
+    })
+
+
+    document.getElementById('Q2A').addEventListener('click', function () {
+        plusQuestion(1);
+        overMan +=1; 
+    })
+    document.getElementById('Q2B').addEventListener('click', function () {
+        plusQuestion(1);
+        regularMan += 1; 
+    })
+    document.getElementById('Q2C').addEventListener('click', function () {
+        plusQuestion(1);
+        lastMan += 1; 
+    })
+
+
+
+    document.getElementById('Q3A').addEventListener('click', function () {
+        plusQuestion(1);
+        regularMan += 1; 
+    })
+    document.getElementById('Q3B').addEventListener('click', function () {
+        plusQuestion(1);
+        overMan += 1; 
+    })
+    document.getElementById('Q3C').addEventListener('click', function () {
+        plusQuestion(1);
+        lastMan += 1; 
+    })
+
+
+    document.getElementById('Q4A').addEventListener('click', function () {
+        plusQuestion(1);
+        lastMan += 1; 
+    })
+    document.getElementById('Q4B').addEventListener('click', function () {
+        plusQuestion(1);
+        overMan += 1; 
+    })
+    document.getElementById('Q4C').addEventListener('click', function () {
+        plusQuestion(1);
+        regularMan += 1; 
+    })
+
+
+    document.getElementById('Q5A').addEventListener('click', function () {
+        tabulateResult(); 
+        lastMan += 1; 
+        plusQuestion(1);
+    })
+    document.getElementById('Q5B').addEventListener('click', function () {
+        tabulateResult(); 
+        regularMan += 1; 
+        plusQuestion(1);
+    })
+    document.getElementById('Q5C').addEventListener('click', function () {
+        tabulateResult(); 
+        overMan += 1; 
+        plusQuestion(1);
+    })
+
+    document.getElementById('retake').addEventListener('click', function(){
+        questionIndex = 1; 
+        overMan = 0; 
+        regularMan = 0; 
+        lastMan = 0; 
+        plusQuestion(1)
+
+    })
+
+    let tabulatedResult; 
+
+    function tabulateResult(){
+
+ 
+    if(lastMan > regularMan && lastMan > overMan){
+        document.getElementById('results-div').style.backgroundImage = 'url(https://images.unsplash.com/photo-1578916171728-46686eac8d58?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80)'
+        tabulatedResult = ' You are the Last Man, Nietzsches greatest fear for humanity\'s future. However, knowing Nietzsche, that\'s probably not such a bad thing.'
+    } 
+    if(overMan > regularMan && overMan > lastMan){
+        document.getElementById('results-div').style.backgroundImage = 'url(https://images.unsplash.com/photo-1532444458054-01a7dd3e9fca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2224&q=80)'
+
+        tabulatedResult = 'You are a true Nietzschean, or Overman, one who creates new values to vanquish nihilism. Enjoy!'
+    }
+
+    if (regularMan > overMan && regularMan >= lastMan || regularMan > lastMan && regularMan >= overMan) {
+        document.getElementById('results-div').style.backgroundImage = 'url(https://images.unsplash.com/photo-1590343365416-788d3e16a065?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1334&q=80)'
+
+        tabulatedResult = 'Not a Nietzschean, just an average joe  :('
+    }
+        document.getElementById('result').innerHTML = tabulatedResult; 
+}
+
+    
     
 });
 
